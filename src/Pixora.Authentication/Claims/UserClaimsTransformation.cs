d3f0df8b0590f1ff1bf2d4739db6cb2218f8d5bd
N1K0232 <ns.nicolasilvestri@libero.it>
2025-12-20 15:00:59 +0100

    Add ClaimsTransformer

---
+++ src/Pixora.Authentication/Claims/UserClaimsTransformation.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Pixora.Authentication.Entities;
+
+namespace Pixora.Authentication.Claims;
+
+public class UserClaimsTransformation(UserManager<ApplicationUser> userManager) : IClaimsTransformation
+{
+    public async Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+    {
+        var user = await userManager.GetUserAsync(principal);
+        if (user is not null)
+        {
+            var claims = await userManager.GetClaimsAsync(user);
+            if (claims.Count == 0)
+            {
+                await userManager.AddClaimsAsync(user, principal.Claims);
+            }
+        }
+
+        return principal;
+    }
+}
