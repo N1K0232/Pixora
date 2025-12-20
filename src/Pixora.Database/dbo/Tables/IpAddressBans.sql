2d1f168b162b5e0a5158d5134c21f0ba8febe4ed
N1K0232 <ns.nicolasilvestri@libero.it>
2025-12-20 22:01:01 +0100

    Add IP address ban

---
+++ src/Pixora.Database/dbo/Tables/IpAddressBans.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[IpAddressBans]
+(
+	[Id]        UNIQUEIDENTIFIER          NOT NULL,
+    [Value]     NVARCHAR (255)            NOT NULL,
+    [Reason]    NVARCHAR (4000)           NULL,
+    [BannedAt]  DATETIMEOFFSET (7)        NOT NULL,
+    [ExpiresAt] DATETIMEOFFSET (7)        NULL
+);
+
+GO
+ALTER TABLE [dbo].[IpAddressBans]
+ADD CONSTRAINT [PK_IpAddressBans] PRIMARY KEY ([Id] ASC);
+
+GO
+ALTER TABLE [dbo].[IpAddressBans]
+ADD CONSTRAINT [DF_IpAddressBans_Id] DEFAULT (NEWSEQUENTIALID()) FOR [Id];
